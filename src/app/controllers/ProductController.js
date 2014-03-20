ac1dbdef6e5fed51c02e10f91909c3775a0d9b42
Andre Baltieri <andrebaltieri@hotmail.com>
2014-03-20 00:44:15 -0300

    Somente HTML

---
+++ src/app/controllers/ProductController.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    'use strict';
+
+    var controllerId = 'ProductController';
+
+    // TODO: replace app with your module name
+    angular.module('app').controller(controllerId,
+        ['$scope', ProductController]);
+
+    function ProductController($scope) {
+        var vm = this;
+
+        vm.activate = activate;
+        vm.title = 'ProductController';
+
+        function activate() { }
+    }
+})();
---
+++ src/app/routes.js
@@ -0,0 +1,31 @@
+﻿(function () {
+    'use strict';
+    var id = 'app';
+
+    var app = angular.module('app', [
+        'ngAnimate',
+        'ngRoute'
+    ]);
+
+    app.config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                controller: 'HomeController as vm',
+                templateUrl: 'app/views/home/index.html'
+            })
+            .when('/produtos', {
+                controller: 'ProductController as vm',
+                templateUrl: 'app/views/product/index.html'
+            })
+            .when('/categorias', {
+                controller: 'CategoryController as vm',
+                templateUrl: 'app/views/category/index.html'
+            });
+    });
+
+    app.config(['$httpProvider', function ($httpProvider) {
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    }]);
+
+})();
